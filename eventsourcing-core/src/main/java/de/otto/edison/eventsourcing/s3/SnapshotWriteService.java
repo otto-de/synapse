86c19ce9dd93144b56a6d2442eff33771ce5ccc0
Ralf Warfelmann <ralf.warfelmann@otto.de>
2018-01-18 11:50:32 +0100

    refactor snapshot deletion

--- eventsourcing-core/src/main/java/de/otto/edison/eventsourcing/s3/SnapshotWriteService.java
+++ eventsourcing-core/src/main/java/de/otto/edison/eventsourcing/s3/SnapshotWriteService.java
@@ -67,15 +67,15 @@ public class SnapshotWriteService {
                         final StreamPosition currentStreamPosition,
                         final StateRepository<String> stateRepository) throws IOException {
         File snapshotFile = createSnapshotFile(streamName);
+        FileOutputStream fos = new FileOutputStream(snapshotFile);
+        BufferedOutputStream bos = new BufferedOutputStream(fos);
+        ZipOutputStream zipOutputStream = new ZipOutputStream(bos);
+        ZipEntry zipEntry = new ZipEntry(ZIP_ENTRY);
+        zipEntry.setMethod(ZipEntry.DEFLATED);
+        zipOutputStream.putNextEntry(zipEntry);
+        JsonGenerator jGenerator = jsonFactory.createGenerator(zipOutputStream, JsonEncoding.UTF8);
 
-        try (FileOutputStream fos = new FileOutputStream(snapshotFile);
-             BufferedOutputStream bos = new BufferedOutputStream(fos);
-             ZipOutputStream zipOutputStream = new ZipOutputStream(bos)
-        ) {
-            ZipEntry zipEntry = new ZipEntry(ZIP_ENTRY);
-            zipEntry.setMethod(ZipEntry.DEFLATED);
-            zipOutputStream.putNextEntry(zipEntry);
-            JsonGenerator jGenerator = jsonFactory.createGenerator(zipOutputStream, JsonEncoding.UTF8);
+        try {
             jGenerator.writeStartObject();
             writeSequenceNumbers(currentStreamPosition, jGenerator);
             // write to data file
@@ -95,12 +95,17 @@ public class SnapshotWriteService {
 
             jGenerator.writeEndArray();
             jGenerator.writeEndObject();
-            jGenerator.flush();
-            zipOutputStream.closeEntry();
         } catch (Exception e) {
             LOG.info("delete file {}", snapshotFile.toPath().toString());
             deleteFile(snapshotFile);
             throw e;
+        } finally {
+            fos.close();
+            bos.close();
+            jGenerator.flush();
+            jGenerator.close();
+            zipOutputStream.closeEntry();
+            zipOutputStream.close();
         }
         return snapshotFile;
     }
