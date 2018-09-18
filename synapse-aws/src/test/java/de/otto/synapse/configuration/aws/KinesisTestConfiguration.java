2b906cb15a7f7608a125398b8e4695f2637d5884
Guido Steinacker <guido.steinacker@otto.de>
2018-09-18 17:08:39 +0200

    Increased timeouts for Kinesis

--- synapse-aws/src/test/java/de/otto/synapse/configuration/aws/KinesisTestConfiguration.java
+++ synapse-aws/src/test/java/de/otto/synapse/configuration/aws/KinesisTestConfiguration.java
@@ -10,10 +10,16 @@ import org.springframework.context.annotation.Primary;
 import software.amazon.awssdk.auth.credentials.AwsCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.core.client.builder.ClientHttpConfiguration;
+import software.amazon.awssdk.http.SdkHttpClient;
+import software.amazon.awssdk.http.SdkHttpClientFactory;
+import software.amazon.awssdk.http.apache.ApacheSdkHttpClientFactory;
+import software.amazon.awssdk.http.nio.netty.NettySdkHttpClientFactory;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.kinesis.KinesisClient;
 
 import java.net.URI;
+import java.time.Duration;
 
 import static de.otto.synapse.endpoint.MessageInterceptorRegistration.receiverChannelsWith;
 import static org.slf4j.LoggerFactory.getLogger;
@@ -43,7 +49,12 @@ public class KinesisTestConfiguration implements MessageEndpointConfigurer {
         System.setProperty("aws.cborEnabled", "false");
         LOG.info("kinesis client for local tests");
         if (testEnvironment.equals("local")) {
+            final SdkHttpClientFactory factory = ApacheSdkHttpClientFactory.builder()
+                    .connectionTimeout(Duration.ofSeconds(10))
+                    .socketTimeout(Duration.ofSeconds(10))
+                    .build();
             return builder()
+                    .httpConfiguration(ClientHttpConfiguration.builder().httpClientFactory(factory).build())
                     .endpointOverride(URI.create("http://localhost:4568"))
                     .region(Region.EU_CENTRAL_1)
                     .credentialsProvider(StaticCredentialsProvider.create(
