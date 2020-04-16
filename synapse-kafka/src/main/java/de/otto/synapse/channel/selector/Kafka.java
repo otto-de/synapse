12504c1f6c8b1383deddfb111c4e5dadc3dd0a7c
Guido Steinacker <guido.steinacker@otto.de>
2020-04-16 14:07:29 +0200

    Adds KafkaMessageSenderEndpointFactory implementation

---
+++ synapse-kafka/src/main/java/de/otto/synapse/channel/selector/Kafka.java
@@ -0,0 +1,6 @@
+package de.otto.synapse.channel.selector;
+
+/**
+ * {@link Selector} used to specify that the desired endpoint is a Kafka message log.
+ */
+public interface Kafka extends MessageLog {}
---
+++ synapse-kafka/src/main/java/de/otto/synapse/endpoint/sender/kafka/KafkaMessageSenderEndpointFactory.java
@@ -0,0 +1,35 @@
+package de.otto.synapse.endpoint.sender.kafka;
+
+import de.otto.synapse.channel.selector.Kafka;
+import de.otto.synapse.channel.selector.Selector;
+import de.otto.synapse.endpoint.MessageInterceptorRegistry;
+import de.otto.synapse.endpoint.sender.MessageSenderEndpoint;
+import de.otto.synapse.endpoint.sender.MessageSenderEndpointFactory;
+import de.otto.synapse.translator.MessageFormat;
+import de.otto.synapse.translator.TextMessageTranslator;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import javax.annotation.Nonnull;
+
+public class KafkaMessageSenderEndpointFactory implements MessageSenderEndpointFactory {
+
+    private final MessageInterceptorRegistry registry;
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaMessageSenderEndpointFactory(final MessageInterceptorRegistry registry,
+                                             final KafkaTemplate<String, String> kafkaTemplate) {
+        this.registry = registry;
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @Override
+    public MessageSenderEndpoint create(final @Nonnull String channelName, final MessageFormat messageFormat) {
+        return new KafkaMessageSender(channelName, registry, new TextMessageTranslator(), kafkaTemplate);
+    }
+
+    @Override
+    public boolean matches(final Class<? extends Selector> channelSelector) {
+        return channelSelector.isAssignableFrom(Kafka.class);
+    }
+
+}
