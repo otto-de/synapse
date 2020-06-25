5ae28d139c271b12c0ce362fbccd0881cfd92d25
mgeissen <matthias.geissendoerfer@otto.de>
2020-06-25 08:39:14 +0200

    update documentation for release script
    Co-authored-by: Matthias Geißendörfer <matthias.geissendoerfer@otto.de>

--- release.sh
+++ release.sh
@@ -18,9 +18,10 @@ check_configuration() {
     check_configured "sonatypeUsername" "This is the username you use to authenticate with sonatype nexus (e.g. otto-de)"
     check_configured "sonatypePassword" "This is the password you use to authenticate with sonatype nexus (ask Guido or one of the developers)"
     check_configured "signing.secretKeyRingFile" "This is the gpg secret key file, e.g. ~/.gnupg/secring.gpg. If this doesn't exist, generate a key: gpg --gen-key"
-    check_configured "signing.keyId" "This is the id of your key (e.g. 72FE5380). Use gpg --list-keys to find yours"
+    check_configured "signing.keyId" "This is the id of your key (e.g. 72FE5380). Use gpg --list-keys (gpg version >= 2.1: gpg --list-keys --keyid-format short -> use short key with 8 characters) to find yours"
     check_configured "signing.password" "This is the password you defined for your gpg key"
     # gpg --send-keys --keyserver keyserver.ubuntu.com yourKeyId
+    # For gpg version >= 2.1: gpg --keyring secring.gpg --export-secret-keys > ~/.gnupg/secring.gpg
 }
 
 check_configuration
